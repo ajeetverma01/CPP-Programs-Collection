2044d7fe6a85cc98688ad4079583b0b25baccb2c
Ajeet Verma <av401402@gmail.com>
2025-03-13 19:04:52 +0530

    SwapingUsingPointer.cpp

---
+++ SwapingUsingPointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    swap(&x, &y);
+    cout << "After swapping: " << x << " " << y << endl;
+    return 0;
+}
